f1de210e560c112b182ce8db376512fc79ae3e53
Amit Kumar Das <amit.inverse@gmail.com>
2024-04-17 21:56:27 +0530

    Initial commit

---
+++ main.js
@@ -0,0 +1,75 @@
+// console.log("Hello World!")
+
+const { app, BrowserWindow, Menu, globalShortcut } = require('electron');
+
+// Set environment
+process.env.NODE_ENV = 'development';
+
+const isDev = process.env.NODE_ENV !== 'production' ? true : false;
+// console.log(process.platform)
+const isMac = process.platform === 'darwin' ? true : false;
+
+let mainWindow;
+
+function createMainWindow() {
+    mainWindow = new BrowserWindow({
+        title: 'Image Shrink',
+        width: 500,
+        height: 600,
+        icon: './assets/icons/Icon_256x256.png',
+        resizable: isDev ? true : false,
+        backgroundColor: 'white',
+    });
+
+    // mainWindow.loadURL('https://twitter.com')
+    // mainWindow.loadURL(`file://${__dirname}/app/index.html`)
+    mainWindow.loadFile('./app/index.html');
+}
+
+// app.on('ready', createMainWindow);
+app.on('ready', () => {
+    createMainWindow();
+
+    const mainMenu = Menu.buildFromTemplate(menu);
+    Menu.setApplicationMenu(mainMenu);
+
+    globalShortcut.register('CmdOrCtrl+R', () => mainWindow.reload());
+    globalShortcut.register(isMac ? 'Command+Alt+I' : 'Ctrl+Shift+I', () => mainWindow.toggleDevTools());
+
+    mainWindow.on('ready', () => (mainWindow = null));
+});
+
+const menu = [
+    ...(isMac ? [{ role: 'appMenu' }] : []),
+    {
+        label: 'File',
+        submenu: [
+            {
+                label: 'Quit',
+                // accelerator: isMac ? 'Command+W' : 'Ctrl+W',
+                accelerator: 'CmdOrCtrl+W',
+                click: () => app.quit(),
+            },
+        ],
+    },
+];
+
+// if (isMac) {
+//     menu.unshift({
+//         role: 'appMenu'
+//     });
+// }
+
+app.on('window-all-closed', () => {
+    if (!isMac) {
+        app.quit();
+    }
+});
+
+app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+        createMainWindow();
+    }
+});
+
+app.allowRendererProcessReuse = true;
