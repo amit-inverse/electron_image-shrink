49b810488ae85316a71892f68212f53a3e8ca2a2
Amit Kumar Das <amit.inverse@gmail.com>
2024-04-18 07:43:37 +0530

    ...

--- main.js
+++ main.js
@@ -1,75 +1,67 @@
-// console.log("Hello World!")
-
-const { app, BrowserWindow, Menu, globalShortcut } = require('electron');
+const { app, BrowserWindow, Menu, globalShortcut } = require("electron");
 
 // Set environment
-process.env.NODE_ENV = 'development';
+process.env.NODE_ENV = "development";
 
-const isDev = process.env.NODE_ENV !== 'production' ? true : false;
-// console.log(process.platform)
-const isMac = process.platform === 'darwin' ? true : false;
+const isDev = process.env.NODE_ENV !== "production" ? true : false;
+const isMac = process.platform === "darwin" ? true : false;
 
 let mainWindow;
 
 function createMainWindow() {
-    mainWindow = new BrowserWindow({
-        title: 'Image Shrink',
-        width: 500,
-        height: 600,
-        icon: './assets/icons/Icon_256x256.png',
-        resizable: isDev ? true : false,
-        backgroundColor: 'white',
-    });
+  mainWindow = new BrowserWindow({
+    title: "Image Shrink",
+    width: 500,
+    height: 600,
+    icon: "./assets/icons/Icon_256x256.png",
+    resizable: isDev ? true : false,
+    backgroundColor: "white",
+  });
 
-    // mainWindow.loadURL('https://twitter.com')
-    // mainWindow.loadURL(`file://${__dirname}/app/index.html`)
-    mainWindow.loadFile('./app/index.html');
+  // mainWindow.loadURL('https://twitter.com')
+  // mainWindow.loadURL(`file://${__dirname}/app/index.html`)
+  mainWindow.loadFile("./app/index.html");
 }
 
-// app.on('ready', createMainWindow);
-app.on('ready', () => {
-    createMainWindow();
+app.on("ready", () => {
+  createMainWindow();
 
-    const mainMenu = Menu.buildFromTemplate(menu);
-    Menu.setApplicationMenu(mainMenu);
+  const mainMenu = Menu.buildFromTemplate(menu);
+  Menu.setApplicationMenu(mainMenu);
 
-    globalShortcut.register('CmdOrCtrl+R', () => mainWindow.reload());
-    globalShortcut.register(isMac ? 'Command+Alt+I' : 'Ctrl+Shift+I', () => mainWindow.toggleDevTools());
+  globalShortcut.register("CmdOrCtrl+R", () => mainWindow.reload());
+  globalShortcut.register(isMac ? "Command+Alt+I" : "Ctrl+Shift+I", () =>
+    mainWindow.toggleDevTools()
+  );
 
-    mainWindow.on('ready', () => (mainWindow = null));
+  mainWindow.on("ready", () => (mainWindow = null));
 });
 
 const menu = [
-    ...(isMac ? [{ role: 'appMenu' }] : []),
-    {
-        label: 'File',
-        submenu: [
-            {
-                label: 'Quit',
-                // accelerator: isMac ? 'Command+W' : 'Ctrl+W',
-                accelerator: 'CmdOrCtrl+W',
-                click: () => app.quit(),
-            },
-        ],
-    },
+  ...(isMac ? [{ role: "appMenu" }] : []),
+  {
+    label: "File",
+    submenu: [
+      {
+        label: "Quit",
+        // accelerator: isMac ? 'Command+W' : 'Ctrl+W',
+        accelerator: "CmdOrCtrl+W",
+        click: () => app.quit(),
+      },
+    ],
+  },
 ];
 
-// if (isMac) {
-//     menu.unshift({
-//         role: 'appMenu'
-//     });
-// }
-
-app.on('window-all-closed', () => {
-    if (!isMac) {
-        app.quit();
-    }
+app.on("window-all-closed", () => {
+  if (!isMac) {
+    app.quit();
+  }
 });
 
-app.on('activate', () => {
-    if (BrowserWindow.getAllWindows().length === 0) {
-        createMainWindow();
-    }
+app.on("activate", () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createMainWindow();
+  }
 });
 
 app.allowRendererProcessReuse = true;
